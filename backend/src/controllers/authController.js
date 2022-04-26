bf38d0ad29183f39c325d489c627cdf99cbdfa95
David de Sousa Alves <david.alves@saude.ce.gov.br>
2022-04-26 13:04:47 -0300

    Retorna token do servidor e compara hash da senha

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,8 +1,15 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 function doLogin(req, res, next) {
     const {email, password} = req.body;
 
-    if (email === 'davidsousaalves@gmail.com' && password === '123456') {
-        res.sendStatus(200);
+    if (email === 'davidsousaalves@gmail.com' &&
+        bcrypt.compareSync(password, '$2a$12$pRJNLtnJMufrWa01oeYE5uirutvE4ayOCuMXoNnKuu5bl0VlulTTm')) {
+        const token = jwt.sign({id: 1}, process.env.JWT_SECRET, {
+            expiresIn: parseInt(process.env.JWT_EXPIRES)
+        })
+        res.json({ token });
     } else {
         res.sendStatus(401);
     }
